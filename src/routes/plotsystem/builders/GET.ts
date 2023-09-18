338ac782b9d97d84928d12e6fafae6cf883a4242
MineFact <minefact.minecraft@gmail.com>
2023-09-18 10:18:51 -0500

    fixed GET builders

--- src/routes/plotsystem/builders/GET.ts
+++ src/routes/plotsystem/builders/GET.ts
@@ -5,7 +5,14 @@ export async function initRoutes(app: Router, joi: any, plotSystem: PlotSystem) 
 
     app.get('/api/plotsystem/builders', function (req, res) {
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(plotSystem.getPSBuilders()))
+        res.send(JSON.stringify(plotSystem.getPSBuilders(), replacer));
     })
 
+}
+
+function replacer(key: string, value: any) {
+    if (typeof value === 'bigint') {
+        return value.toString();
+    }
+    return value;
 }
